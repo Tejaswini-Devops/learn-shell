6a22447b89adc972e814bbc2a544fbe0020df395
tejudevops <saitejaswini8121@gmail.com>
2025-03-29 14:25:04 +0530

    we are learning all concepts of shell scripting

--- exit.sh
+++ exit.sh
@@ -1,11 +1,13 @@
 sample1()
 {
   echo hello1
-  exit 100
+  return 100
   echo hello2
 }
 sample1
-echo exit status of function -$?
+echo exit status of function - $?
+
+
 echo hello1
 exit 200
 echo hello2
