4b5ba30f9d3106f2053f6b9bba217ba7ee029798
tejudevops <saitejaswini8121@gmail.com>
2025-03-29 10:56:31 +0530

    we are learning all concepts of shell scripting

---
+++ 2.print.sh
@@ -0,0 +1,20 @@
+# here we talked about the details od colors we can print in the cmd prompt
+
+# We have six colors
+# Red    -31
+# green  -32
+# yellow -33
+# blue   -34
+# magenta -35
+# Cyan    -36
+
+#syntax
+
+#    echo -e "\e[colorcodemMessage\e[0m"
+
+echo -e "\e[31mHelloWorld\e[0m"
+echo -e "\e[32mHelloWorld\e[0m"
+echo -e "\e[33mHelloWorld\e[0m"
+echo -e "\e[34mHelloWorld\e[0m"
+echo -e "\e[35mHelloWorld\e[0m"
+echo -e "\e[36mHelloWorld\e[0m"
